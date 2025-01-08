3e24020a9b600be71fb084f39435f7c0de228678
Robson Nonato <108809490+NonattoDev@users.noreply.github.com>
2025-01-08 15:38:05 -0300

    Adiciona integração com a API Chartmetric para obter ranking de artistas e implementa gerenciamento de token de acesso

--- app/artistas/Actions/GetArtistasRanking.ts
+++ app/artistas/Actions/GetArtistasRanking.ts
@@ -1,6 +1,15 @@
 "use server";
 
+import ChartMetricIntegrador from "@/lib/Chartmetric";
+
 export default async function GetArtistasRanking() {
+  const response = await ChartMetricIntegrador.get("artist/sp_followers/list?min=500&max=10000&offset=0&code2=BR&city=Chicago", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  console.log(response.data.obj.data);
   return [
     { id: 1, name: "The Beatles", rank: 1 },
     { id: 2, name: "Elton John", rank: 2 },
--- lib/Chartmetric.ts
+++ lib/Chartmetric.ts
@@ -1,6 +1,50 @@
 import axios from "axios";
 
+const refreshToken = "ctnHv2yBnJwuHBslUlvtHEaFDwBN0fAPLG2eqo6ChDYzay3eHXbVZRc5sALZ3RyH";
+
 const ChartMetricIntegrador = axios.create({
   baseURL: "https://api.chartmetric.com/api",
-  headers: {},
 });
+
+// Variável para armazenar o token de acesso
+let accessToken: string = "";
+
+// Função para gerar um novo token de acesso
+async function generateAccessToken(): Promise<string> {
+  const response = await axios.post("https://api.chartmetric.com/api/token", { refreshtoken: refreshToken }, { headers: { "Content-Type": "application/json" } });
+  accessToken = response.data.token; // Assumindo que o token vem como `response.data.token`
+  return accessToken;
+}
+
+// Interceptor para adicionar o token de acesso a todas as requisições
+ChartMetricIntegrador.interceptors.request.use(
+  async (config) => {
+    if (!accessToken) {
+      // Gera um token se não existir
+      accessToken = await generateAccessToken();
+    }
+    // Adiciona o token no cabeçalho Authorization
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Interceptor para tratar erros de autenticação
+ChartMetricIntegrador.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      // Gera um novo token caso o atual expire
+      accessToken = await generateAccessToken();
+      // Repete a requisição original com o novo token
+      error.config.headers.Authorization = `Bearer ${accessToken}`;
+      return axios(error.config);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default ChartMetricIntegrador;
